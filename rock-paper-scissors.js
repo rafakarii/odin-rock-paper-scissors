62cfd6bc4cacc4f5ad562981e313b099c52d5cd9
Rafail <73640317+rafakarii@users.noreply.github.com>
2023-06-24 20:33:20 +0600

    Add function game(). Edit func playRound()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,7 +34,7 @@ function playRound(playerSelection, computerSelection) {
                 return tieResult;
             }
         default:
-            return 'Choose: Rock, Paper, Scissors'; 
+            return 'Invalid Prompt'; 
     }
 }
         
@@ -42,5 +42,46 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
+function game(rounds) {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < rounds; i++) {
+        let invalidInput = true;
+        let playerSelection = '';
+        while (invalidInput) {
+            playerSelection = prompt('Choose: Rock, Paper or Scissors. To end the game type Exit').toLowerCase();
+            if (
+                (playerSelection === 'rock') ||
+                (playerSelection === 'paper') ||
+                (playerSelection === 'scissors') ||
+                (playerSelection === 'exit')
+            ) {
+                invalidInput = false;
+            }
+        }
+        if (playerSelection === 'exit') {
+            break;
+        }
+         
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
 
-playRound('paper', getComputerChoice());
+        if (roundResult.startsWith('You Win')) {
+            playerScore++;
+        } else if (roundResult.startsWith('You Lose')) {
+            computerScore++;
+        }
+    }
+
+    let gameScore = `Score ${playerScore} : ${computerScore}`;
+    let gameResult = '';
+    if (playerScore > computerScore) {
+        gameResult = 'You win! ';
+    } else if (playerScore < computerScore) {
+        gameResult = 'You lose! ';
+    } else {
+        gameResult = "It's a tie! ";
+    }
+    console.log('Game over. ' + gameResult + gameScore);
+}
